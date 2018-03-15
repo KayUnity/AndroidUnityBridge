87e50a7f3f7c367327754ab2f529d778eccfab56
Kay01010101 <407068578@qq.com>
2018-03-15 11:23:57 +0800

    de

--- AndroidUnityBridge/src/com/iky/aub/BridgeConstant.java
+++
@@ -1,6 +0,0 @@
-package com.iky.aub;
-
-public class BridgeConstant
-{
-	public final static String SplitFlag = "__=__";
-}
--- AndroidUnityBridge/src/com/iky/aub/app/BridgeApp.java
+++
@@ -1,6 +0,0 @@
-package com.iky.aub.app;
-
-public interface BridgeApp
-{
-	void Request(String funcName, String params);
-}
